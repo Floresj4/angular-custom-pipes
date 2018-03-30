f656da783a6b206b90806d1b3246279462d95e0a
Jason <floresj4@gmail.com>
2018-03-30 18:00:07 -0400

    starting filter pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,17 +12,34 @@ export class AppComponent implements OnInit {
   limit: number;
 
   shortenForm: FormGroup;
+  filterForm: FormGroup;
+
+  filterData: any[];
 
   ngOnInit() {
     this.limit = 30;
+
+    this.filterData = [
+      { name: 'John Doe', age: 27 },
+      { name: 'Angela Doe', age: 33 },
+      { name: 'Molly Doe', age: 24 },
+    ];
 
     this.shortenForm = new FormGroup({
       'limitInput': new FormControl(this.limit, Validators.min(1)),
       'shortenText': new FormControl(null)
     });
+
+    this.filterForm = new FormGroup({
+      'filterText': new FormControl(null)
+    });
   }
 
   getShortenCmd() {
     return '{{shortenMsg | shorten:limit}}';
+  }
+
+  getFilterCmd() {
+    return '{{filterList}}';
   }
 }
