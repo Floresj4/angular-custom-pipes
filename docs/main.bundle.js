a526da2148f5761e37c0832db0090afbd574c83d
Jason <floresj4@gmail.com>
2018-04-01 15:36:01 -0400

    updating github page

--- docs/main.bundle.js
+++ docs/main.bundle.js
@@ -25,7 +25,7 @@ exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-b
 
 
 // module
-exports.push([module.i, "input.ng-invalid.ng-touched {\r\n  border:1px solid #f00;\r\n}\r\n", ""]);
+exports.push([module.i, "input.ng-invalid.ng-touched {\r\n  border:1px solid #f00;\r\n}\r\n\r\n.label-adjust {\r\n  margin-top: 24px;\r\n}\r\n", ""]);
 
 // exports
 
@@ -38,7 +38,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/app.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"container\">\n  <div class=\"row\">\n    <div class=\"col-xs-12\">\n      <div class=\"page-header\">\n        <h3>Angular <small>Custom Pipes</small></h3>\n      </div>\n    </div>\n  </div>\n\n  <div class=\"row\">\n      <div class=\"col-xs-12\">\n        <h4>Shorten</h4>\n        <p>\n          A custom shortening pipe truncating text longer then 30 characters, but also parameterized to allow\n          the length to be configured.  The textarea below binds to the variable <span class=\"code\">shortenMsg</span>\n          and displays in the following container. <code [innerHTML]=\"getShortenCmd()\"></code>\n        </p>\n      </div>\n  </div>\n\n  <div class=\"row\">\n    <form [formGroup]=\"shortenForm\">\n\n    <div class=\"col-xs-3\">\n      <label for=\"limitInput\">Limit</label>\n      <input type=\"number\"\n             id=\"limitInput\"\n             name=\"limitInput\"\n             formControlName=\"limitInput\"\n             class=\"form-control\"\n             [(ngModel)]=\"limit\">\n    </div>\n    <div class=\"col-xs-9\">\n        <label for=\"shortenText\">Input text</label>\n        <textarea\n          id=\"shortenText\"\n          name=\"shortenText\"\n          formControlName=\"shortenText\"\n          class=\"form-control\"\n          row=\"4\"\n          cols=\"20\"\n          [(ngModel)]=\"shortenMsg\">\n            </textarea>\n    </div>\n\n    </form>\n  </div>\n\n  <div class=\"row\">\n    <div class=\"col-xs-12\">\n      <h5>Output <small>limit: {{limit}}</small></h5>\n      <p *ngIf=\"shortenForm.valid; else invalidForm\">{{shortenMsg | shorten:limit }} &nbsp;</p>\n      <ng-template #invalidForm>\n        <div class=\"alert alert-danger\" role=\"alert\">Shorten limit must be a positive, non-zero number.</div>\n      </ng-template>\n    </div>\n  </div>\n\n  <div class=\"row\">\n    <div class=\"col-xs-12\">\n      <h4>Filter</h4>\n      <p>\n        A custom filter pipe... <br/> <code [innerHTML]=\"getFilterCmd()\"></code>\n      </p>\n    </div>\n  </div>\n\n  <div class=\"row\">\n    <form [formGroup]=\"filterForm\">\n\n      <div class=\"col-xs-3\">\n        <div class=\"form-group\">\n          <label for=\"filterText\">Filter</label>\n          <input type=\"text\"\n              id=\"filterText\"\n              name=\"filterText\"\n              [(ngModel)]=\"filterText\"\n              formControlName=\"filterText\"\n              class=\"form-control\">\n        </div>\n\n        <div class=\"form-group\">\n          <label for=\"filterProp\">Property</label>\n          <select class=\"form-control\"\n            id=\"filterProp\"\n            name=\"filterProp\"\n            [(ngModel)]=\"filterProp\"\n            formControlName=\"filterProp\">\n            <option *ngFor=\"let p of filterProperties\">{{p}}</option>\n          </select>\n        </div>\n      </div>\n\n      <div class=\"col-xs-9\">\n        <label>Filter Data</label>\n        <ul class=\"list-group\">\n          <li class=\"list-group-item\" *ngFor=\"let d of filterData | filter:filterText:filterProp\">\n            {{ d.name }}, {{ d.age }}, <span class=\"label label-default\">{{ d.occupation }}</span></li>\n        </ul>\n      </div>\n\n    </form>\n  </div>\n\n  <div class=\"row\" style=\"margin-top:25px\">\n    <div class=\"col-xs-12\">\n      <div class='list-group'>\n        <a class='list-group-item' href='https://floresj4.github.io/'>\n          <span class='glyphicon glyphicon-home'></span> &nbsp; floresj4.github.io</a>\n        <a class='list-group-item' href='https://github.com/Floresj4/angular-custom-pipes'>\n          <span class='glyphicon glyphicon-console'></span> &nbsp; https://github.com/Floresj4/angular-custom-pipes</a>\n      </div>\n    </div>\n  </div>\n</div>\n"
+module.exports = "<div class=\"container\">\n  <div class=\"row\">\n    <div class=\"col-xs-12\">\n      <div class=\"page-header\">\n        <h3>Angular <small>Custom Pipes</small></h3>\n      </div>\n    </div>\n  </div>\n\n  <div class=\"row\">\n      <div class=\"col-xs-12\">\n        <h4>Shorten</h4>\n        <p>\n          A custom shortening pipe truncating text longer then 30 characters, but also parameterized to allow\n          the length to be configured.  The textarea below binds to the variable <span class=\"code\">shortenMsg</span>\n          and displays in the following container. <br/> <code [innerHTML]=\"getShortenCmd()\"></code>\n        </p>\n      </div>\n  </div>\n\n  <div class=\"row\">\n    <form [formGroup]=\"shortenForm\">\n\n    <div class=\"col-xs-3\">\n      <label for=\"limitInput\">Limit</label>\n      <input type=\"number\"\n             id=\"limitInput\"\n             name=\"limitInput\"\n             formControlName=\"limitInput\"\n             class=\"form-control\"\n             [(ngModel)]=\"limit\">\n    </div>\n    <div class=\"col-xs-9\">\n        <label for=\"shortenText\">Input text</label>\n        <textarea\n          id=\"shortenText\"\n          name=\"shortenText\"\n          formControlName=\"shortenText\"\n          class=\"form-control\"\n          row=\"4\"\n          cols=\"20\"\n          [(ngModel)]=\"shortenMsg\">\n            </textarea>\n    </div>\n\n    </form>\n  </div>\n\n  <div class=\"row\">\n    <div class=\"col-xs-12\">\n      <h5>Output <small>limit: {{limit}}</small></h5>\n      <p *ngIf=\"shortenForm.valid; else invalidForm\">{{shortenMsg | shorten:limit }} &nbsp;</p>\n      <ng-template #invalidForm>\n        <div class=\"alert alert-danger\" role=\"alert\">Shorten limit must be a positive, non-zero number.</div>\n      </ng-template>\n    </div>\n  </div>\n\n  <div class=\"row\">\n    <div class=\"col-xs-12\">\n      <h4>Filter</h4>\n      <p>\n        A custom filter pipe for collections allowing users to specify the text to filter along with the property name of a\n        Javascript object to filter against.\n        <br/> <code [innerHTML]=\"getFilterCmd()\"></code>\n      </p>\n    </div>\n  </div>\n\n  <form [formGroup]=\"filterForm\">\n    <div class=\"row\">\n        <div class=\"col-xs-3\">\n          <div class=\"form-group\">\n            <label for=\"filterText\">Filter</label>\n            <input type=\"text\"\n                id=\"filterText\"\n                name=\"filterText\"\n                [(ngModel)]=\"filterText\"\n                formControlName=\"filterText\"\n                class=\"form-control\">\n          </div>\n\n          <div class=\"form-group\">\n            <label for=\"filterProp\">Property</label>\n            <select class=\"form-control\"\n              id=\"filterProp\"\n              name=\"filterProp\"\n              [(ngModel)]=\"filterProp\"\n              formControlName=\"filterProp\">\n              <option *ngFor=\"let p of filterProperties\">{{p}}</option>\n            </select>\n          </div>\n        </div>\n\n        <div class=\"col-xs-9\">\n          <label>Filter Data</label>\n          <ul class=\"list-group\">\n            <li class=\"list-group-item\" *ngFor=\"let d of filterData | filter:filterText:filterProp\">\n              {{ d.name }}, {{ d.age }}, <span class=\"label label-default\">{{ d.occupation }}</span></li>\n          </ul>\n        </div>\n    </div>\n  </form>\n\n  <form [formGroup]=\"filterFormAdd\">\n    <div class=\"row\">\n\n      <div class=\"col-xs-4\">\n        <label for=\"nameField\">Name</label>\n        <input type=\"text\"\n          id=\"nameField\"\n          name=\"nameField\"\n          formControlName=\"name\"\n          placeholder=\"First and last...\"\n          class=\"form-control\"\n          required>\n      </div>\n\n      <div class=\"col-xs-3\">\n        <label for=\"ageField\">Age</label>\n        <input type=\"number\"\n          id=\"ageField\"\n          name=\"ageField\"\n          formControlName=\"age\"\n          class=\"form-control\"\n          required>\n      </div>\n\n      <div class=\"col-xs-3\">\n        <label for=\"occupationField\">Occupation</label>\n        <input type=\"text\"\n          id=\"occupationField\"\n          name=\"occupataionField\"\n          formControlName=\"occupation\"\n          placeholder=\"Job title...\"\n          class=\"form-control\"\n          required>\n      </div>\n\n      <div class=\"col-xs-2\">\n        <button type=\"button\"\n          class=\"btn btn-primary label-adjust\"\n          (click)=\"onAddData()\">Add</button>\n      </div>\n    </div>\n  </form>\n\n  <div class=\"row\" style=\"margin-top:25px\">\n    <div class=\"col-xs-12\">\n      <div class='list-group'>\n        <a class='list-group-item' href='https://floresj4.github.io/'>\n          <span class='glyphicon glyphicon-home'></span> &nbsp; floresj4.github.io</a>\n        <a class='list-group-item' href='https://github.com/Floresj4/angular-custom-pipes'>\n          <span class='glyphicon glyphicon-console'></span> &nbsp; https://github.com/Floresj4/angular-custom-pipes</a>\n      </div>\n    </div>\n  </div>\n</div>\n"
 
 /***/ }),
 
@@ -80,12 +80,23 @@ var AppComponent = (function () {
             'filterText': new __WEBPACK_IMPORTED_MODULE_1__angular_forms__["a" /* FormControl */](null),
             'filterProp': new __WEBPACK_IMPORTED_MODULE_1__angular_forms__["a" /* FormControl */](null)
         });
+        this.filterFormAdd = new __WEBPACK_IMPORTED_MODULE_1__angular_forms__["b" /* FormGroup */]({
+            'name': new __WEBPACK_IMPORTED_MODULE_1__angular_forms__["a" /* FormControl */](null),
+            'age': new __WEBPACK_IMPORTED_MODULE_1__angular_forms__["a" /* FormControl */](null, __WEBPACK_IMPORTED_MODULE_1__angular_forms__["e" /* Validators */].min(1)),
+            'occupation': new __WEBPACK_IMPORTED_MODULE_1__angular_forms__["a" /* FormControl */](null)
+        });
     };
     AppComponent.prototype.getShortenCmd = function () {
         return '{{shortenMsg | shorten:limit}}';
     };
     AppComponent.prototype.getFilterCmd = function () {
         return '*ngFor="let i of items | filter:filterText:propName';
+    };
+    AppComponent.prototype.onAddData = function () {
+        if (this.filterFormAdd.valid) {
+            this.filterData.push(this.filterFormAdd.value);
+            this.filterFormAdd.reset();
+        }
     };
     AppComponent = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
@@ -167,24 +178,37 @@ var FilterPipe = (function () {
     function FilterPipe() {
     }
     FilterPipe.prototype.transform = function (value, filterString, propName) {
-        if (value.length === 0 || filterString === undefined || filterString === '') {
+        // ensure the value or filter string is valid
+        if (value.length === 0 || this.isFilterValid(filterString)) {
             return value;
         }
+        // adjust the case for property lookup
         if (propName !== undefined) {
             propName = propName.toLowerCase();
+        }
+        if (filterString !== undefined) {
+            filterString = filterString.toLowerCase();
         }
         var results = [];
         for (var _i = 0, value_1 = value; _i < value_1.length; _i++) {
             var item = value_1[_i];
-            if (item[propName].toLowerCase() === filterString.toLowerCase()) {
+            // compare values as strings for simplicity
+            var itemValue = item[propName].toString()
+                .toLowerCase();
+            // use a contains approach to filter
+            if (itemValue.indexOf(filterString) > -1) {
                 results.push(item);
             }
         }
         return results;
     };
+    FilterPipe.prototype.isFilterValid = function (filterString) {
+        return filterString === undefined || filterString === '';
+    };
     FilterPipe = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["O" /* Pipe */])({
-            name: 'filter'
+            name: 'filter',
+            pure: false
         })
     ], FilterPipe);
     return FilterPipe;
