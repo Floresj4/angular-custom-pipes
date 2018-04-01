18aedb83f999609282e015414bc8cfd5c5bf5a1f
Jason <floresj4@gmail.com>
2018-04-01 14:13:06 -0400

    updating to check contains

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -6,21 +6,28 @@ import {Pipe, PipeTransform} from '@angular/core';
 export class FilterPipe implements PipeTransform {
 
   transform(value: any, filterString: string, propName: string): any {
-    if(value.length === 0 || filterString === undefined || filterString === '') {
+    // ensure the value or filter string is valid
+    if(value.length === 0 || this.isFilterValid(filterString)) {
       return value;
     }
 
+    // adjust the case for property lookuip
     if(propName !== undefined) {
       propName = propName.toLowerCase();
     }
 
     const results = [];
     for(const item of value) {
-      if(item[propName].toLowerCase() === filterString.toLowerCase()) {
+      // use a contains approach to filter
+      if(item[propName].toLowerCase().indexOf(filterString.toLowerCase()) > -1) {
         results.push(item);
       }
     }
 
     return results;
+  }
+
+  isFilterValid(filterString: string) {
+    return filterString === undefined || filterString === '';
   }
 }
